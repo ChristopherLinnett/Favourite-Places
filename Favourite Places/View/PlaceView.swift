e18a806ef54aba4632d130c138eb1019ddcbb905
ChristopherLinnett <chrislinnett45389@gmail.com>
2022-05-09 20:01:21 +1000

    Add image URL downloader

--- Favourite Places/View/PlaceView.swift
+++ Favourite Places/View/PlaceView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+let defaultImage = Image(systemName: "photo")
 
 struct PlaceView: View {
     @Environment(\.editMode) var editMode
@@ -35,7 +36,7 @@ struct PlaceView: View {
                     }
                 }
             } else {
-                Text(place.placeImageURL != "" ? place.placeImageURL : "noURL")
+                image(for: place.place_image_URL).aspectRatio(contentMode: .fit)
                 Text(place.placeDescription != "" ? place.placeDescription: "noDescription")
                 VStack{
                     Text("lat: \(place.placeLatitude)")
@@ -44,6 +45,11 @@ struct PlaceView: View {
             }
         }.navigationTitle(place.placeTitle)
     }
-    
+    func image(for url: URL?) -> Image {
+        guard let url = url,
+              let data = try? Data(contentsOf: url),
+              let uiImg = UIImage(data: data) else {return defaultImage}
+        return Image(uiImage: uiImg).resizable()
+    }
 }
 
