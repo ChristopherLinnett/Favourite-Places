0fb4438d614a01c68fab16fcf429dc4fee3c9618
ChristopherLinnett <chrislinnett45389@gmail.com>
2022-05-09 18:09:30 +1000

    Add ViewModel

---
+++ Favourite Places/ViewModel/PlaceViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  PlaceViewModel.swift
+//  Favourite Places
+//
+//  Created by Christopher Linnett on 9/5/2022.
+//
+import CoreData
+import Foundation
+import SwiftUI
+
+extension Place {
+    ///non-optional ViewModel Properties for (optionals) 'place_title' `place_description` database attributes
+    var placeTitle: String {
+        get { place_title ?? ""}
+        set { place_title = newValue }
+    }
+    var placeDescription: String {
+        get { place_description ?? ""}
+        set { place_description = newValue}
+    }
+    var placeImageURL: String {
+        get { place_image_URL?.absoluteString ?? "" }
+        set { place_image_URL = URL.init(string: newValue)}
+    }
+}
