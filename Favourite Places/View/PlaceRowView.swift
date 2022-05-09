875b5729ae2952e9618f82ac57740686c6c15530
ChristopherLinnett <chrislinnett45389@gmail.com>
2022-05-09 21:15:34 +1000

    add image to main row view with loader

--- Favourite Places/View/PlaceRowView.swift
+++ Favourite Places/View/PlaceRowView.swift
@@ -9,12 +9,15 @@ import SwiftUI
 
 struct PlaceRowView: View {
     @ObservedObject var place: Place
+    @State var image = Image(systemName: "photo").resizable()
     var body: some View {
         HStack {
-            if let url = place.place_image_URL {
-                Text("URL: \(url.absoluteString)")
-            }
+            image.aspectRatio(contentMode: .fit)
+                .frame(width: 80, height: 40, alignment: .center)
             Text(place.placeTitle)
     }
+        .task {
+            image = await place.getImage()
+        }
     }
 }
