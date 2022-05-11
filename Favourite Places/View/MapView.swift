a426bdfce3f6595bc0d00806f7b2256ca0ba7542
ChristopherLinnett <chrislinnett45389@gmail.com>
2022-05-11 20:53:32 +1000

    Create new view for hosting map Add Mapkit framework to project

---
+++ Favourite Places/View/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  Favourite Places
+//
+//  Created by Christopher Linnett on 11/5/2022.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
