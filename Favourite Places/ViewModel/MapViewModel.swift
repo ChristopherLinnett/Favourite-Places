a9b93e2b0187cc8d1cf8363f97211dbcaa977511
ChristopherLinnett <chrislinnett45389@gmail.com>
2022-05-11 23:14:35 +1000

    create map view model extension for having a string bound to the lat/lon of the region in the map view

---
+++ Favourite Places/ViewModel/MapViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  MapViewModel.swift
+//  Favourite Places
+//
+//  Created by Christopher Linnett on 11/5/2022.
+//
+
+import Foundation
+import MapKit
+
+extension MKCoordinateRegion {
+    var latitudeString: String {
+        get {String(center.latitude)
+        }
+        set {guard let lat = CLLocationDegrees(newValue) else {return}
+            center.latitude = lat
+        }
+        }
+    var longitudeString: String {
+        get {String(center.longitude)
+        }
+        set {guard let lon = CLLocationDegrees(newValue) else {return}
+            center.longitude = lon
+        }
+}
+}
