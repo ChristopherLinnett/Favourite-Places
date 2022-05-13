4b7b8aeccfca1bb642b541cc9d38984834d503c3
ChristopherLinnett <chrislinnett45389@gmail.com>
2022-05-13 13:42:17 +1000

    remove boolean output from send data functions as app will delete textfield no matter the outcome

--- Favourite Places/ViewModel/MapViewModel.swift
+++ Favourite Places/ViewModel/MapViewModel.swift
@@ -12,33 +12,27 @@ extension MKCoordinateRegion {
     var latitudeString: String {
         get {String(center.latitude)
         }
-        set {guard let lat = CLLocationDegrees(newValue) else {return}
+        set {guard let lat = CLLocationDegrees(newValue) else { return }
             center.latitude = lat
         }
-        }
+    }
     var longitudeString: String {
         get {String(center.longitude)
         }
-        set {guard let lon = CLLocationDegrees(newValue) else {return}
+        set {guard let lon = CLLocationDegrees(newValue) else { return }
             center.longitude = lon
         }
-}
-    @discardableResult
-    mutating func sendToMapLat(latitude: String)->Bool {
-        guard let lat = Float(latitude) else {return false}
+    }
+    mutating func sendToMapLat(latitude: String) {
+        guard let lat = Float(latitude) else {return}
         if lat >= -90 && lat <= 90 {
-        latitudeString = latitude
-        } else {return false}
-        return true
+            latitudeString = latitude
+        } else { return }
     }
-    @discardableResult
-    mutating func sendToMapLon(longitude: String)->Bool {
-        guard let lon = Float(longitude) else {return false}
+    mutating func sendToMapLon(longitude: String){
+        guard let lon = Float(longitude) else {return}
         if lon >= -180 && lon <= 180 {
-        longitudeString = longitude
-        } else {return false}
-        return true
-
+            longitudeString = longitude
+        } else { return }
     }
-    
 }
