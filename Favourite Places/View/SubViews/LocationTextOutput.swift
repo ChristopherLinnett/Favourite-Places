e08268aa53feb75b4036cdad5f130c3d1aa382e6
ChristopherLinnett <chrislinnett45389@gmail.com>
2022-05-23 17:57:57 +1000

    gave geocode button some padding

--- Favourite Places/View/SubViews/LocationTextOutput.swift
+++ Favourite Places/View/SubViews/LocationTextOutput.swift
@@ -14,10 +14,15 @@ struct LocationTextOutput: View {
             Button {
                 print("finding co-ords")
             } label: {
-                Image(systemName: "magnifyingglass.circle").resizable()
-            }.frame(width: 30, height: 30, alignment: .leading)
+                Image(systemName: "magnifyingglass.circle").resizable(resizingMode: .stretch).padding(.all, 12.0)
+            }.frame(width: 50, height: 50, alignment: .leading)
             TextField("Noosa Heads", text: $locationName).font(.title2).frame(height: 50)
         }
     }
 }
 
+struct LocationTextOutput_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationTextOutput(locationName: "Big Ben")
+    }
+}
